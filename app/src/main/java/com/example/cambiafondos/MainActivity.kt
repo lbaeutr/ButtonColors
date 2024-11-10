b0170053c883757cc06153f622ec4780c059deee
lbaeutr <lbaeutr@gmail.com>
2024-11-10 10:09:30 +0100

    Correciones

--- app/src/main/java/com/example/cambiafondos/MainActivity.kt
+++ app/src/main/java/com/example/cambiafondos/MainActivity.kt
@@ -15,6 +15,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val mainLayout = findViewById<View>(R.id.main)
         val cambiarColorBoton = findViewById<Button>(R.id.button)
